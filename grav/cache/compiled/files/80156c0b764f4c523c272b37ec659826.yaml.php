9fd5c62b28cd4f8b4488c4e63fde0ed1e09a983e
Adam Spotton <aspotton@gmail.com>
2019-04-07 14:39:12 -0400

    Purged errant cache files

--- grav/cache/compiled/files/80156c0b764f4c523c272b37ec659826.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://saturn/saturn.yaml',
-    'modified' => 1553199717,
-    'data' => [
-        'enabled' => true
-    ]
-];
